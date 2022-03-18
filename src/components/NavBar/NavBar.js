8db28d5b76a1faee31a81c08d907ac5b07e5ad4c
danielon9320 <daniel_ls93@live.com.ar>
2022-03-18 17:58:30 -0300

    navbar finalizado

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,40 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./NavBar.css";
+
+function NavBar() {
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
+  <img/>
+    <a class="navbar-brand m-1" href="#">Vynil Store</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav mx-auto">
+        <li class="nav-item">
+          <a class="nav-link " href="#">Vinilos</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">CD's</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Cassettes</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Otros</a>
+        </li>
+      </ul>
+    </div>
+    <div>
+      <button type="button" class="btn btn-primary">Suscribite</button>
+    </div>
+  </div>
+</nav>
+    </>
+  );
+}
+
+export default NavBar;
