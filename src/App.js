e3c6e5c3ae6e4c42502eeb2acc2fd24e0b1567ca
danielon9320 <daniel_ls93@live.com.ar>
2022-03-26 23:16:43 -0300

    item count done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
-
-import './App.css';
-
-import ItemListContainer from './components/ItemListContainer/ItemListContainer.jsx';
-import NavBar from './components/NavBar/NavBar';
+import "./App.css";
+import ItemListContainer from "./components/ItemListContainer/ItemListContainer.jsx";
+import NavBar from "./components/NavBar/NavBar";
 function App() {
   return (
     <div className="App">
-      <NavBar/>
-      <ItemListContainer greeting={"Hola"}/>
-      
+      <NavBar />
+      <ItemListContainer greeting={"Hola"} />
     </div>
   );
 }
--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import ItemCount from '../ItemCount/ItemCount'
 
 const ItemListContainer = ({greeting}) => {
+
+  const onAdd = (quantity) => {
+    console.log(quantity)
+  }
   return (
-    <><h1>{greeting}</h1></>
+    <>
+    <h1>{greeting}</h1>
+    <ItemCount initial={1} stock={20} onAdd={onAdd}/>
+    </>
   )
 }
 
