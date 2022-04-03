6e4c779c402a95b3d91fb8f6a906706dbd09bc27
danielon9320 <daniel_ls93@live.com.ar>
2022-04-03 19:05:22 -0300

    correcion de componentes

--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -1,9 +1,76 @@
-import React from 'react'
+import React from "react";
 
-const ItemDetail = () => {
+const ItemDetail = (prod) => {
   return (
-    <div>ItemDetail</div>
-  )
-}
+    <>
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <h1
+              style={{
+                textAlign: "center",
+                marginTop: "1em",
+                marginBottom: "1em",
+              }}
+            >
+              {prod.name}
+            </h1>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-lg-6 mb-4">
+            <div className="product-img">
+              <img src={prod.img} alt="producto" className="img-fluid" />
+            </div>
+          </div>
+          <div className="col-lg-6">
+            <div className="form-horizontal bg-light p-4">
+              <div className="form-group row">
+                <label className="col-sm-3 col-md-3 form-control-label nopaddingtop">
+                  Precio:
+                </label>
+                <div className="col-sm-8 col-md-9">
+                  <span className="product-form-price">us$ {prod.price}</span>
+                </div>
+              </div>
 
-export default ItemDetail
+              <div className="form-group row">
+                <label className="col-sm-3 col-md-3 form-control-label">
+                  Stock:
+                </label>
+                <div className="col-sm-8 col-md-9">
+                  <span>{prod.stock}</span>
+                </div>
+              </div>
+
+              <div className="form-group row">
+                <label className="col-sm-3 col-md-3 form-control-label">
+                  Descripcion:
+                </label>
+                <div className="col-sm-8 col-md-9 description">
+                  <p>{prod.description}</p>
+                </div>
+              </div>
+
+              <div className="form-group row">
+                <label className="col-sm-3 col-md-3 form-control-label">
+                  Categoria:
+                </label>
+                <div className="col-sm-9 col-md-9">
+                  <p>{prod.category}</p>
+                </div>
+              </div>
+              <div className="form-group row">
+                <div className="col-sm-9 col-md-9">
+                  <button className="btn btn-primary">Comprar ahora!</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ItemDetail;
