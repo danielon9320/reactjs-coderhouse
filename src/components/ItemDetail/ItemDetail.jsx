2f5f1c8423fc7202189dd06bc1cfd834d4f204bd
danielon9320 <daniel_ls93@live.com.ar>
2022-04-04 00:09:29 -0300

    mejorando estilos en item detail para entrega

--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -1,73 +1,29 @@
 import React from "react";
+import './ItemDetail.css'
+
 
 const ItemDetail = (prod) => {
   return (
     <>
-      <div className="container">
-        <div className="row">
-          <div className="col-12">
-            <h1
-              style={{
-                textAlign: "center",
-                marginTop: "1em",
-                marginBottom: "1em",
-              }}
-            >
-              {prod.name}
-            </h1>
-          </div>
+      <div key={prod.id} className="container mt-5">
+      <div className="row justify-content-center text-center align-items-center">
+        <div className="col-lg-6 col-md-12 col-sm-12 pb-5">
+          <img src={prod.img} className="itemDetail-img" alt="Imagen Producto"></img>
         </div>
-        <div className="row">
-          <div className="col-lg-6 mb-4">
-            <div className="product-img">
-              <img src={prod.img} alt="producto" className="img-fluid" />
-            </div>
-          </div>
-          <div className="col-lg-6">
-            <div className="form-horizontal bg-light p-4">
-              <div className="form-group row">
-                <label className="col-sm-3 col-md-3 form-control-label nopaddingtop">
-                  Precio:
-                </label>
-                <div className="col-sm-8 col-md-9">
-                  <span className="product-form-price">us$ {prod.price}</span>
-                </div>
-              </div>
+        <div className="col-lg-6 col-md-12 col-sm-12">
+          <h3 className="fw-bold">{prod.name}</h3>
+          <h4>${prod.price}</h4>
+          <p>{prod.description}</p>
+          <h5>Stock: {prod.stock}</h5>
+          <div className="d-flex justify-content-center">
+          
+                                <button className="btn btn-danger bg-gradient me-3 mt-3">Ir al carrito</button>
+                           
 
-              <div className="form-group row">
-                <label className="col-sm-3 col-md-3 form-control-label">
-                  Stock:
-                </label>
-                <div className="col-sm-8 col-md-9">
-                  <span>{prod.stock}</span>
-                </div>
-              </div>
-
-              <div className="form-group row">
-                <label className="col-sm-3 col-md-3 form-control-label">
-                  Descripcion:
-                </label>
-                <div className="col-sm-8 col-md-9 description">
-                  <p>{prod.description}</p>
-                </div>
-              </div>
-
-              <div className="form-group row">
-                <label className="col-sm-3 col-md-3 form-control-label">
-                  Album:
-                </label>
-                <div className="col-sm-9 col-md-9">
-                  <p>{prod.category}</p>
-                </div>
-              </div>
-              <div className="form-group row">
-                <div className="col-sm-9 col-md-9">
-                  <button className="btn btn-primary">Comprar</button>
-                </div>
-              </div>
-            </div>
-          </div>
         </div>
+      </div>
+
+      </div>
       </div>
     </>
   );
