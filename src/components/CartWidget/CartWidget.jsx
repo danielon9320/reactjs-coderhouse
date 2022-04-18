7b569bcca4ef6c77a0895b6deffd571c03d54a1a
danielon9320 <daniel_ls93@live.com.ar>
2022-04-18 08:54:51 -0300

    implementando mejoras. correcion de compilacion

--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import { useContext } from 'react';
 import { Link } from 'react-router-dom'
-import CartContext from '../../context/CartContext'
+import {CartContext }from '../../context/CartContext'
 import "./CartWidget.css";
 
 const CartWidget = () => {
   const { getQuantity } = useContext(CartContext)
   return (
     <>
-    <Link to={'/'}>
+    <Link to={'/cart'}>
           <img src={require('../assets/img/shopping-cart.png')} class="w-50" id="img" alt="imagen carrito"></img>
           { getQuantity() }
           </Link>
---
+++ src/components/ItemCart/ItemCart.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { useContext } from 'react';
+import  { CartContext }   from '../../context/CartContext';
+
+const ItemCart = (product) => {
+  const { removeItem } = useContext(CartContext);
+  return (
+    <div key={product.id} className="itemCart">
+    <div>
+        <img src={product.img} alt={product.name}/>
+    </div>
+    <div >
+        <h2>{product.name}</h2>
+        <p> Cantidad: {product.quantity} </p>
+    </div>
+    <div>
+    <button onClick={()=> removeItem(product.id)}>Eliminar Producto</button>
+    </div>
+
+</div>
+  )
+}
+
+export default ItemCart
